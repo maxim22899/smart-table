56fa87b02483abd95b5608c39711a57e9689c56a
Maxim <ulyanin.maksym@yandex.ru>
2026-09-15 22:45:44 +0300

    new commit

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,11 +1,19 @@
-import {rules, createComparison} from "../lib/compare.js";
+import {rules, defaultRules, createComparison} from "../lib/compare.js";
 
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
+    const compare = createComparison({
+            ...defaultRules,
+            ...rules.searchMultipleFields(searchField, ['date','customer', 'seller'], false)
+        }
+    );
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        if (!action || action.name !== 'search') {
+            return data;
+        }
+        return data.filter(row => compare(row, state));
     }
-}
+}  
--- src/main.js
+++ src/main.js
@@ -9,6 +9,15 @@ import {processFormData} from "./lib/utils.js";
 import {initTable} from "./components/table.js";
 // @todo: подключение
 
+import { initPagination } from './components/pagination.js';
+
+import { initSorting } from './components/sorting.js';
+import { initFiltering } from './components/filtering.js';
+import { initSearching } from './components/searching.js';
+
+
+
+
 
 // Исходные данные используемые в render()
 const {data, ...indexes} = initData(sourceData);
@@ -20,8 +29,13 @@ const {data, ...indexes} = initData(sourceData);
 function collectState() {
     const state = processFormData(new FormData(sampleTable.container));
 
+    const rowsPerPage = parseInt(state.rowsPerPage);
+    const page = parseInt(state.page ?? 1);
+
     return {
-        ...state
+        ...state,
+        rowsPerPage,
+        page
     };
 }
 
@@ -34,6 +48,12 @@ function render(action) {
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
 
+    result = applySearching(result, state, action);
+
+    result = applyFiltering(result, state, action);
+    result = applySorting(result, state, action);
+
+    result = applyPagination(result, state, action);
 
     sampleTable.render(result)
 }
@@ -41,11 +61,34 @@ function render(action) {
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: [],
-    after: []
+    before: ['search','header', 'filter'],
+    after: ['pagination']
 }, render);
 
 // @todo: инициализация
+
+const applyPagination = initPagination(
+    sampleTable.pagination.elements,
+    (el, page, isCurrent) => {
+        const input = el.querySelector('input');
+        const label = el.querySelector('span');
+        input.value = page;
+        input.checked = isCurrent;
+        label.textContent = page;
+        return el;
+    }
+);
+
+const applySorting = initSorting([
+    sampleTable.header.elements.sortByDate,
+    sampleTable.header.elements.sortByTotal
+]);
+
+const applyFiltering = initFiltering(sampleTable.filter.elements, {
+    searchBySeller: indexes.sellers
+});
+
+const applySearching = initSearching(sampleTable.search.elements.search);
 
 
 const appRoot = document.querySelector('#app');
